b4e15add56760dac45c9866ad27410a4b3f1edb8
Lily <lmayhenderson@yahoo.com>
2024-05-01 17:11:08 -0700

    Add alert when sold out.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,8 +22,7 @@ class OrdersController < ApplicationController
   private
 
   def empty_cart!
-    # empty hash means no products in cart :)
-    update_cart({})
+    session[:cart] = {}
   end
 
   def perform_stripe_charge
