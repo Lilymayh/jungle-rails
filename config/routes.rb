4da2c62fc42edcea264a4b0b8ec61b122c5811f2
Lily <lmayhenderson@yahoo.com>
2024-05-08 22:45:49 -0700

    Add login, logout and register routes.

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,11 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
-  
+  post '/login' => 'sessions#create'
+get '/logout' => 'sessions#destroy'
+
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
