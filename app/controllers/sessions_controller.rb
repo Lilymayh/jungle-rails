e0b9c6cd9b6f6eaa8f90f6a52d8470f87a8adedb
Lily <lmayhenderson@yahoo.com>
2024-05-08 23:36:27 -0700

    Fix login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+    if user = User.authenticate_with_credentials(params[:session][:email], params[:session][:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
