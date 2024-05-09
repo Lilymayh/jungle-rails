4dfbfa3de801f12d6385b9d03fde337b1a20445d
Lily <lmayhenderson@yahoo.com>
2024-05-08 22:35:34 -0700

    Backup commit

--- config/routes.rb
+++ config/routes.rb
@@ -18,20 +18,9 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
-    
-    # I've created a gif controller so I have a page I can secure later. 
-    # This is optional (as is the root to: above).
+  
     get '/cool' => 'gif#cool'
     get '/sweet' => 'gif#sweet'
-
-    get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-get '/logout' => 'sessions#destroy'
-
-    # These routes will be for signup. The first renders a form in the browse, the second will 
-    # receive the form and create a user in our database using the data given to us by the user.
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
 end
   
   # The priority is based upon order of creation: first created -> highest priority.
